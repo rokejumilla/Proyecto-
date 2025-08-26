2c8766461a8d150113f5e7d7458f19ee146c1496
rokejumilla <santinojumilla16@gmail.com>
2025-08-26 10:32:36 -0300

    players.scripts

---
+++ proyecto/Assets/Scripts/player-2.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player-2 : MonoBehaviour
+{
+    // Variables a configurar desde el editor
+    [Header("Configuracion")]
+    [SerializeField] float velocidad = 5f;
+
+    // Variables de uso interno en el script
+    private float moverHorizontal;
+    private float moverVertical;
+    private Vector2 direccion;
+
+    // Variable para referenciar otro componente del objeto
+    private Rigidbody2D miRigidbody2D;
+
+    // Codigo ejecutado cuando el objeto se activa en el nivel
+    private void OnEnable()
+    {
+        miRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Codigo ejecutado en cada frame del juego (Intervalo variable)
+    private void Update()
+    {
+        moverHorizontal = Input.GetAxis("Horizontal");
+        moverVertical = Input.GetAxis("Vertical");
+        direccion = new Vector2(moverHorizontal, moverVertical);
+    }
+    private void FixedUpdate()
+    {
+        miRigidbody2D.MovePosition(miRigidbody2D.position + direccion * (velocidad * Time.fixedDeltaTime));
+    }
+}
+
